3e4539df5b2460db617706ed0285ad0790e4d2b9
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-03-18 17:46:12 -0400

    add: Resume link

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,28 +3,50 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa"
+import { Link } from "gatsby"
 
-const IndexPage = () => (
-  <Layout pageId="home" bgImageClass="bgImage">
-    <SEO title="Home" />
-    <h1 className="lg-heading">
-      Mike <span className="text-secondary">McManus</span>
-    </h1>
-    <h2 className="sm-heading">
-      Fullstack Web Developer, Software Engineer & Entrepreneur
-    </h2>
-    <div className="icons">
-      <a href="https://www.instagram.com/mike_givesyouhell/">
-        <FaInstagram size="2em" />
-      </a>
-      <a href="https://www.linkedin.com/in/mikemcmanus95/">
-        <FaLinkedin size="2em" />
-      </a>
-      <a href="https://github.com/MikeMcmanus95">
-        <FaGithub size="2em" />
-      </a>
-    </div>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const resumePath = data?.allFile?.edges[0]?.node?.publicURL
+
+  return (
+    <Layout pageId="home" bgImageClass="bgImage">
+      <SEO title="Home" />
+      <h1 className="lg-heading">
+        Mike <span className="text-secondary">McManus</span>
+      </h1>
+      <h2 className="sm-heading">
+        Fullstack Web Developer, Software Engineer & Entrepreneur ||{" "}
+        <span>
+          <Link className="text-secondary" to={resumePath} target="_blank">
+            Full Resume
+          </Link>
+        </span>
+      </h2>
+      <div className="icons">
+        <a href="https://www.instagram.com/mike_givesyouhell/">
+          <FaInstagram size="2em" />
+        </a>
+        <a href="https://www.linkedin.com/in/mikemcmanus95/">
+          <FaLinkedin size="2em" />
+        </a>
+        <a href="https://github.com/MikeMcmanus95">
+          <FaGithub size="2em" />
+        </a>
+      </div>
+    </Layout>
+  )
+}
+
+export const indexQuery = graphql`
+  {
+    allFile(filter: { extension: { eq: "pdf" } }) {
+      edges {
+        node {
+          publicURL
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -34,24 +34,6 @@ const Projects = ({ data }) => {
   )
 }
 
-//  {
-//    data.allMarkdownRemark.edges.map(post => (
-//      <div key={post.node.id}>
-//        <h3>{post.node.frontmatter.title}</h3>
-//        <small>
-//          Posted by {post.node.frontmatter.author} on{" "}
-//          {post.node.frontmatter.date}
-//        </small>
-//        <br />
-//        <br />
-//        <Link to={post.node.frontmatter.path}>Read more</Link>
-//        <br />
-//        <br />
-//        <hr />
-//      </div>
-//    ))
-//  }
-
 export const projQuery = graphql`
   query projectIndexQuery {
     allMarkdownRemark {
