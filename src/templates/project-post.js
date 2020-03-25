128f5021f6ccf4097d000671e954be322e85e812
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-03-25 01:00:27 -0400

    test: remove font awesome icons

--- src/templates/project-post.js
+++ src/templates/project-post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import SEO from "../components/seo"
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa"
+// import { FaChevronLeft, FaChevronRight } from "react-icons/fa"
 
 const ProjectPost = ({ data }) => {
   const post = data.markdownRemark
@@ -10,22 +10,22 @@ const ProjectPost = ({ data }) => {
     <div>
       <SEO title={post.frontmatter.title} />
       <div className="proj-nav">
-        <Link to="/projects">
+        {/* <Link to="/projects">
           <FaChevronLeft size="2em" />
           <FaChevronLeft size="2em" />
-        </Link>
+        </Link> */}
         <div className="title-wrapper">
           <h1>{post.frontmatter.title}</h1>
           <h4>{post.frontmatter.date}</h4>
         </div>
-        {post.frontmatter.next ? (
+        {/* {post.frontmatter.next ? (
           <Link to={post.frontmatter.next}>
             <FaChevronRight size="2em" />
             <FaChevronRight size="2em" />
           </Link>
         ) : (
           <div />
-        )}
+        )} */}
       </div>
       <div
         className="proj-body"
