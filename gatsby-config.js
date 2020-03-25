4b6d13af69f9114b3b90064d3a450fc0368c50b9
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-03-25 16:52:25 -0400

    add: google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,12 @@ module.exports = {
     `gatsby-transformer-json`,
     `gatsby-background-image`,
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+      },
+    },
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
